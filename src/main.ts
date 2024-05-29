6f407a6c2b22283898190b9587912a229f9259c9
Asmovic <asogba007@gmail.com>
2024-05-29 09:42:30 +0100

    Add Swagger Documentation and Allow CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 /* import { CorsOptions } from 'apollo-server-express'; */
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('CashToken Dashboard GraphQL Backend')
+    .setDescription('CashToken Dashboard GraphQL Backend API')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
